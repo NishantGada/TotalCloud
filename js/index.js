66eb645dd286bc3e1d78583b309ed59abcd7f485
Nishant Gada <49140662+NishantGada@users.noreply.github.com>
2021-02-17 11:22:19 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,30 +1,40 @@
-let employee = document.getElementById('employee');
 let assignBtn = document.getElementById('assign');
 let revokeBtn = document.getElementById('revoke');
 let employeeList = document.getElementById('employeeList')
 let assignedEmployees = document.getElementById('assignedEmployees');
 
-function selector() {
-    employee.style.background = "rgb(230, 230, 230)";
-    revokeBtn.style.opacity = 0.4;
-    revokeBtn.style.pointerEvents = "none";
-}
+revokeBtn.style.opacity = 0.4;
+revokeBtn.style.pointerEvents = "none";
 
 function assign() {
-    employee.remove();
+    let employee = document.getElementById('employee');
 
-    var li = document.createElement('li');
-    li.innerHTML = employee.innerHTML;
-    assignedEmployees.appendChild(li);
+    console.log(employee.innerText);
 
-    revokeBtn.style.opacity = 1;
-    revokeBtn.style.pointerEvents = "all";
+    assignBtn.addEventListener('click', function () {
+        employee.remove();
+
+        var li = document.createElement('li');
+        li.innerHTML = employee.innerHTML;
+        assignedEmployees.appendChild(li);
+
+        revokeBtn.style.opacity = 1;
+        revokeBtn.style.pointerEvents = "all";
+    })
+    // var li = document.createElement('li');
+    // li.innerHTML = employee.innerHTML;
+    // assignedEmployees.appendChild(li);
+
+    // revokeBtn.style.opacity = 1;
+    // revokeBtn.style.pointerEvents = "all";
 }
 
 function revoke() {
-    //employee.remove();
+    let employee = document.getElementById('employee');
 
-    assignedEmployees.remove();
+    employee.remove();
+
+    // assignedEmployees.remove();
 
     var li2 = document.createElement('li');
     li2.innerHTML = employee.innerHTML;
